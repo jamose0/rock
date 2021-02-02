386bf701b5c04940f5b55c21a9eaeb2fe4474c59
James M <jmdale@protonmail.com>
2021-02-02 13:44:31 -0600

    Update tests

--- tests/test_file_reader.py
+++ tests/test_file_reader.py
@@ -1,9 +1,10 @@
 import unittest
-from rock_app.data_point import DataPoint
+import rock_app.file_reader as fr
+#from rock_app.data_point import DataPoint
 
 class TestFileReader(unittest.TestCase):
     def test_1(self):
-        self.assertTrue(file_reader() == 5)
+        self.assertTrue(fr.file_to_data() == 5)
 
 if __name__ == "__main__":
     unittest.main()
